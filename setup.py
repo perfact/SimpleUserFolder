47123a035412bbaafc9bed04e0a7269251117978
Jens Hinghaus <jh@perfact.de>
2019-04-08 08:58:02 +0200

    Make ready for PyPI

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '0.1'
 
 setup(name='Products.SimpleUserFolder',
       version=version,
-      description="",
-      long_description="""\
+      description="PerFact Version of the Zope Product SimpleUserFolder",
+      long_description="""The Product SimpleUserFolder allows methods from the
+Data.fs to provide the user list, user addition, modification and deletion,
+and authorization.
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Framework :: Zope :: 2",
+        "Framework :: Zope :: 4"
+      ],
       keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
+      author='PerFact Innovation GmbH & Co. KG',
+      author_email='info@perfact.de',
+      url='https://github.com/perfact/SimpleUserFolder',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       package_data={
-          'Products.SimpleUserFolder': ['www/*',],
+        'Products.SimpleUserFolder': ['www/*', ],
       },
       include_package_data=True,
       zip_safe=False,
